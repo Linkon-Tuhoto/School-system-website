4ef85a0ec4b926636cb080b22cfbfef14794b31d
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-06 23:55:43 +0300

    added scrolling effects for the navbar

--- index.js
+++ index.js
@@ -56,3 +56,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 });
+
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('.header');
+    if(window.scrollY > 50){
+        header.classList.add('scrolled');
+    }
+    else{
+        header.classList.remove('scrolled');
+    }
+})
